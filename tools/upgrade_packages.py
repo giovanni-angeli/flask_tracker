7d5930eeee61b5af5bc5c173fa136130a754f709
giovanni-angeli <giovanni.angeli6@gmail.com>
2021-10-05 14:19:32 +0200

    add tools

---
+++ tools/upgrade_packages.py
@@ -0,0 +1,7 @@
+# coding: utf-8
+
+import pkg_resources
+from subprocess import call
+
+packages = [dist.project_name for dist in pkg_resources.working_set]
+call("pip install --upgrade " + ' '.join(packages), shell=True)
